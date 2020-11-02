1fb62e7216ec00b88d5d8c9d9c324bca547b4bc1
lkubaski <laurent.kubaski@gmail.com>
2020-11-02 18:07:10 +0100

    - renamed 'build' folder to 'ouput' - generated 'js' files added to .gitignore

--- build.sh
+++ build.sh
@@ -2,17 +2,17 @@
 set -e # The script will exit if any command returns a non zero status code
 
 clean() {
-  rm -rf ./build
+  rm -rf ./output
   find ./src/js -name "*.js" -type f -delete
 }
 
 build() {
   tsc # the typescript compiler
-  mkdir ./build
-  cp ./manifest.json ./build
-  cp -r ./src ./build/
-  find ./build -name "*.ts" -type f -delete
-  cd build
+  mkdir ./output
+  cp ./manifest.json ./output
+  cp -r ./src ./output/
+  find ./output -name "*.ts" -type f -delete
+  cd output
   zip -r "yatm.zip" ./* # you can use 'unzip -vl to inspect the zip file content
   cd ..
 }
